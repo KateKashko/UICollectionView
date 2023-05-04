adfbd0fc181cb5b0d373dcc6de47a8099abed526
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-05-04 11:12:58 +0300

    Initial Commit

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Kate Kashko on 4.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
